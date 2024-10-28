6779274312bf99caab2961327726dcdc2f160277
elhedi <hheeddii87@gmail.com>
2024-10-28 17:15:04 +0100

    mise a jour

--- my-boxingCard-app/src/App.tsx
+++ my-boxingCard-app/src/App.tsx
@@ -1,8 +1,54 @@
 import { useState } from 'react'
-import NavBar from './NavBar'
+import NavBar from './components/NavBar'
 import './App.css'
-import Header from './Header';
+import Header from './components/Header';
+import MainCard from './components/MainCard';
 
+
+const fightersList = [
+  {
+    id: 1,
+    nom:"TYSON mike",
+    imgSrc:"images/TYSON.png",
+    poids:"99kg",
+    taille:"178cm"
+  },
+  {
+    id: 2,
+    nom:"ALI muhammad",
+    imgSrc:"images/ALI.png",
+    poids:"97kg",
+    taille:"191cm"
+  },
+  {
+    id: 3,
+    nom:"LEE bruce",
+    imgSrc:"images/LEE.png",
+    poids:"61kg",
+    taille:"172cm"
+  },
+  {
+    id: 4,
+    nom:"VAN DAMME jean-claude",
+    imgSrc:"images/VAN-DAMME.png",
+    poids:"84kg",
+    taille:"177cm"
+  },
+  {
+    id: 5,
+    nom:"BERGER marc-antoine",
+    imgSrc:"",
+    poids:"100kg(de muscles)",
+    taille:"200cm"
+  },
+  {
+    id: 6,
+    nom:"LOPEZ matthieu",
+    imgSrc:"images/croissant.png",
+    poids:"100kg",
+    taille:"195cm"
+  },
+]
 
 function App() {
 
@@ -10,6 +56,9 @@ function App() {
     <div className="document">
     <NavBar />
     <Header />
+    {fightersList.map((fighter) =>(
+      <MainCard data={fighter}/>
+    ))}
     </div>
   )
 }
--- my-boxingCard-app/src/components/MainCard.tsx
+++ my-boxingCard-app/src/components/MainCard.tsx
@@ -1,3 +1,30 @@
-function MainCard (params:type) {
-    
+interface MainCardProps{
+  data:{
+    id: number;
+    nom:string;
+    imgSrc:string;
+    poids:string;
+    taille:string;
+  }
 }
+
+
+
+
+
+
+
+
+function MainCard({data}: MainCardProps) {
+
+    return (
+        <div className="maincard">
+         <img src={data.imgSrc} alt="" />
+         <p>{data.nom}</p>
+         <p>{data.poids}</p>
+         <p>{data.taille}</p>
+        </div>
+    )
+}
+
+export default MainCard
