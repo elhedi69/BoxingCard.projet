fb995298dc75a0b327bd6d7ad9e3dc9434a77886
theo <boronattheo83@gmail.com>
2024-10-29 09:35:51 +0100

    footer test

--- my-boxingCard-app/src/App.tsx
+++ my-boxingCard-app/src/App.tsx
@@ -1,61 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Footer from "./components/footer";
 
 const fighterslist = [
-  {
-    id: 1,
-    nom:"TYSON mike",
-    imgSrc:"",
-    poids:"",
-    taille:""
-  },
-  {
-    id: 1,
-    nom:"ALI muhammad",
-    imgSrc:"",
-    poids:"",
-    taille:""
-  },
-  {
-    id: 1,
-    nom:"LEE bruce",
-    imgSrc:"",
-    poids:"",
-    taille:""
-  },
-  {
-    id: 1,
-    nom:"VAN DAMME jean-claude",
-    imgSrc:"",
-    poids:"",
-    taille:""
-  },
-  {
-    id: 1,
-    nom:"BERGER marc-antoine",
-    imgSrc:"",
-    poids:"",
-    taille:""
-  },
-  {
-    id: 1,
-    nom:"LOPEZ matthieu",
-    imgSrc:"",
-    poids:"",
-    taille:""
-  },
-]
+	{
+		id: 1,
+		nom: "TYSON mike",
+		imgSrc: "",
+		poids: "",
+		taille: "",
+	},
+	{
+		id: 1,
+		nom: "ALI muhammad",
+		imgSrc: "",
+		poids: "",
+		taille: "",
+	},
+	{
+		id: 1,
+		nom: "LEE bruce",
+		imgSrc: "",
+		poids: "",
+		taille: "",
+	},
+	{
+		id: 1,
+		nom: "VAN DAMME jean-claude",
+		imgSrc: "",
+		poids: "",
+		taille: "",
+	},
+	{
+		id: 1,
+		nom: "BERGER marc-antoine",
+		imgSrc: "",
+		poids: "",
+		taille: "",
+	},
+	{
+		id: 1,
+		nom: "LOPEZ matthieu",
+		imgSrc: "",
+		poids: "",
+		taille: "",
+	},
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>BoxingCard</div>
-    </>
-  )
+	return (
+		<div className="document">
+			<NavBar />
+			<Header />
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ my-boxingCard-app/src/components/footer.tsx
@@ -0,0 +1,10 @@
+import gants from "../images/icon-gants-de-boxe.png";
+
+export default function Footer() {
+	return (
+		<div>
+			<img src={gants} alt="gants" />
+			<p>by wilders with love</p>
+		</div>
+	);
+}
